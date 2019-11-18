4e1f387214cc74743407874f6283990335b99775
David Cantú <32000336+codyvi@users.noreply.github.com>
2019-11-17 19:01:12 -0600

    Creado el menu de GameOver y arregle las colisiones

--- Scripts/Choque.cs
+++ Scripts/Choque.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class Choque : MonoBehaviour
 {
@@ -9,37 +11,42 @@ public class Choque : MonoBehaviour
     public Text Txt;
 
     public static int llavesRecogidas;
+    public bool perdiste;
 
     // Start is called before the first frame update
     void Start()
     {
         Txt.text = "Llaves: ";
         llavesRecogidas = 0;
+        perdiste = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Txt.text = "Llaves: " + llavesRecogidas;
+
+        if(perdiste)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            SceneManager.LoadScene(1);
+        }
+
     }
     
     void OnCollisionEnter (Collision col)
     {
-        if(col.gameObject.name == "llave1")
+        if(col.gameObject.name == "llave1" || col.gameObject.name == "llave2" || col.gameObject.name == "llave3")
         {
             llavesRecogidas++;
             Destroy(col.gameObject);
         }
 
-        if(col.gameObject.name == "llave2") 
+        //Dejar asi temporalmente, cambiar cuando esten los niveles 
+        if( (col.gameObject.name == "Puerta1" || col.gameObject.name == "Puerta2" || col.gameObject.name == "Puerta3" ) && llavesRecogidas == 3)
         {
-            llavesRecogidas++;
-            Destroy(col.gameObject);
-        }
-        if(col.gameObject.name == "llave3")
-        {
-            llavesRecogidas++;
-            Destroy(col.gameObject);
+            perdiste = true;
         }
     }
 }
---
+++ Scripts/GameOverMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void VolverAJugar()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+   
+}
